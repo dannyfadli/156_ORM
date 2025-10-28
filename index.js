03ea283593c48a47b062b2497e95f5b861a323c9
danny fadli <danyfdly95@gmail.com>
2025-10-28 14:30:12 +0700

    Konfigurasi parsing body JSON dan URL-encoded

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./models');
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
